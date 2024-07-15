638506f63d1505bb85087c654103802a7ff9422e
Marcin Pilch <marcinpilch4@gmail.com>
2024-07-15 22:32:06 +0200

    Automate yaml test

--- builder/test/test_yaml_builder.cpp
+++ builder/test/test_yaml_builder.cpp
@@ -2,7 +2,7 @@
 
 #include "YamlBuilder.h"
 
-TEST(YamlBuilderTest, VisualTest) {
+TEST(YamlBuilderTest, TestOk) {
   YamlBuilder builder("config");
   builder.AddChild("entry1", "value1");
   builder.AddChild("entry2", "value2");
@@ -23,4 +23,15 @@ TEST(YamlBuilderTest, VisualTest) {
   // Try to add nested element
   fluent_builder.AddChild(yaml_element1);
   std::cout << fluent_builder.str() << std::endl;
+
+  std::string expected_yaml{
+      "fluent:\n"
+      "  - entry1: value1\n"
+      "  - entry2: value2\n"
+      "  fluent:\n"
+      "    - entry1: value1\n"
+      "    - entry2: value2\n"};
+
+  auto fluent_str = fluent_builder.str();
+  EXPECT_EQ(expected_yaml, fluent_str);
 }
