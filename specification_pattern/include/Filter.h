b1fb4f681a258e097ebf79dc5e829324d8cafaf5
Marcin Pilch <marcinpilch4@gmail.com>
2024-07-15 16:22:28 +0200

    Add virtual destructors

--- specification_pattern/include/Filter.h
+++ specification_pattern/include/Filter.h
@@ -4,6 +4,7 @@
 template <typename T>
 class Filter {
  public:
+  virtual ~Filter() = default;
   virtual std::vector<T> filter(std::vector<T> items,
                                 Specification<T>& specification) = 0;
 };
